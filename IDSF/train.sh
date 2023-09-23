fc912fb7df0a7e03fd0df8bd5e06cda31cb48166
chau25102001 <chau.nm194420@sis.hust.edu.vn>
2023-09-23 22:58:37 +0700

    arguments

--- IDSF/train.sh
+++ IDSF/train.sh
@@ -1,4 +1,4 @@
 cd data
 python3 process_data_bio.py
 cd ..
-CUDA_VISIBLE_DEVICES=0 python3 train_bio.py
+CUDA_VISIBLE_DEVICES=0 python3 train_bio.py --train_batch_size 64 --val_batch_size 64
--- text_correction/train.sh
+++ text_correction/train.sh
@@ -1,4 +1,4 @@
 cd data
 python3 prepare_text_correction_data.py
 cd ..
-CUDA_VISIBLE_DEVICES=0 python3 train.py
+CUDA_VISIBLE_DEVICES=0 python3 train.py --train_batch_size 32 --val_batch_size 32
